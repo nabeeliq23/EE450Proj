1f64d41bb6691faa9dad1ce3736f2fa021807f14
Nabeel <nabeeliq@usc.edu>
2023-06-25 01:38:45 -0700

    Client Server Code

--- client.cpp
+++ client.cpp
@@ -1,6 +1,92 @@
 #include <iostream>
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <unistd.h>
+#include  <errno.h>
+#include  <string.h>
+#include  <netdb.h>
+#include  <sys/types.h>
+#include  <netinet/in.h>
+#include  <sys/socket.h>
+#include  <arpa/inet.h>
+#include  <sys/wait.h>
+#include  <signal.h>
+
+#define MAIN_SERVER_PORT 24370
+#define MAIN_SERVER_IP "127.0.0.1"
+
+// bool keepRunning = true;
+
+// void signalHandler(int signum){
+//     if(signum == SIGINT){
+//         keepRunning = false;
+//         close(clientSocket);
+//     }
+// }
 
 int main() {
-    std::cout << "Hello World!";
+
+    // Signal Handler to exit on Ctrl+C
+    // signal(SIGINT, signalHandler);
+
+    // Create a TCP connection
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(clientSocket == -1){
+        std::cerr << "Failed to create client socket." << std::endl;
+        return 1;
+    }
+
+    // Bind client socket dynamically to a port
+    struct sockaddr_in clientAddress;
+    socklen_t clientAddressLength = sizeof(clientAddress);
+    clientAddress.sin_family = AF_INET;
+    clientAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    clientAddress.sin_port = 0; //Dynamic Allocation
+    if(bind(clientSocket, (struct sockaddr *)&clientAddress, sizeof(clientAddress)) < 0){
+        std::cerr << "Binding failed." << std::endl;
+        return 1;
+    }
+
+    // Get the allocated port
+    if(getsockname(clientSocket, (struct sockaddr *)&clientAddress, &clientAddressLength) < 0){
+        std::cerr << "Failed to get the allocated port." << std::endl;
+        return 1;
+    }
+    int clientPort = ntohs(clientAddress.sin_port);
+
+    // Server Setup
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(MAIN_SERVER_PORT);
+    if(inet_pton(AF_INET, MAIN_SERVER_IP, &(serverAddress.sin_addr)) <= 0){
+        std::cerr << "Invalid address." << std::endl;
+    }
+
+    // Connect to the main server
+    if(connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+        std::cerr << "Connection failed." << std::endl;
+        return 1;
+    }
+
+
+    //Continue asking users for input
+   // while(keepRunning) {
+        //Send request to main server
+        std::string request;
+        std::cout << "Please enter the usernames to check the common course selection:";
+        std::getline(std::cin, request);
+        // if(request.empty()){
+        //     continue;
+        // }
+
+        send(clientSocket, request.c_str(), request.size(), 0);
+
+        // Receive response from main server
+        char buffer[1024];
+        int bytesRead = read(clientSocket, buffer, 1024);
+        std::cout << "Response from main server: " << buffer << std::endl;
+
+    //}
+
     return 0;
 }
--- serverM.cpp
+++ serverM.cpp
@@ -1,6 +1,82 @@
 #include <iostream>
+#include <iostream>
+#include  <stdio.h>
+#include  <stdlib.h>
+#include  <unistd.h>
+#include  <errno.h>
+#include  <string.h>
+#include  <netdb.h>
+#include  <sys/types.h>
+#include  <netinet/in.h>
+#include  <sys/socket.h>
+#include  <arpa/inet.h>
+#include  <sys/wait.h>
+#include  <signal.h>
+
+#define MAIN_SERVER_PORT 24370
+
+// bool keepRunning = true; 
 
 int main() {
-    std::cout << "Hello World!";
+     
+    //Create TCP socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0); 
+    if(serverSocket == -1){
+        std::cerr << "Failed to create socket." << std::endl; 
+        return 1; 
+    }
+
+    //Setup Server Address 
+    sockaddr_in serverAddress{}; 
+    serverAddress.sin_family = AF_INET; 
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddress.sin_port = htons(MAIN_SERVER_PORT); 
+
+    //Bind Socket to the specified Address
+    if(bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0){
+        std::cerr << "Binding failed." << std::endl; 
+        return 1; 
+    }
+
+    // Listen for incoming connections
+    if(listen(serverSocket, 1) < 0){
+        std::cerr << "Listening Failed." << std::endl; 
+    }
+
+    std::cout << "Server Listening" << std::endl; 
+
+    //Accept a client connection 
+    int clientSocket; 
+    sockaddr_in clientAddress{}; 
+    socklen_t clientAddressLength = sizeof(clientAddress); 
+
+    clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLength);
+    if(clientSocket < 0){
+        std::cerr << "Failed to accept client connection." << std::endl; 
+        close(serverSocket); 
+        return 1; 
+    }
+
+    std::cout << "Client connected." << std::endl; 
+
+   // while(keepRunning){
+    //Client Request
+    char buffer[1024] = {0}; 
+    int bytesRead = read(clientSocket, buffer, 1024); 
+
+    //Process client request
+    std::string response; 
+    if(bytesRead > 0){
+        std::string request(buffer); 
+        response = "Recieved request: " + request; 
+        std::cout << "Received request from client: " << request << std::endl; 
+    } else {
+        response = "Error: Failed to read client message";
+    }
+    // Send the response to the client
+    send(clientSocket, response.c_str(), response.size(), 0); 
+    std::cout << "Sent response" << std::endl;
+   // } 
+
     return 0;
 }
